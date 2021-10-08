620f44d81eaac544d6e7d7deb7ce8eb68efd12c0
xulegion <73058380+xulegion@users.noreply.github.com>
2021-10-08 17:13:36 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,23 @@ resource "alicloud_instance" "instance"{
       from = "terraform"
     }
 }
+
+resource "alicloud_instance" "this" {
+  ami                    = data.alicloud_ami.ubuntu.id
+  instance_type          = "t3.small"
+  key_name               = "ansible"
+  subnet_id              = alicloud_subnet.subnet.id
+  vpc_security_group_ids = [alicloud_security_group.sg.id]
+  tags = {
+    Name = "ansible-${random_id.hash.hex}"
+  }
+}
+
+resource "ansible_host" "web" {
+  inventory_hostname = alicloud_instance.this.public_ip
+  groups             = ["web"]
+  vars = {
+    port         = 80
+    ansible_user = "ubuntu"
+  }
+}
